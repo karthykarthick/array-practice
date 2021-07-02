b73aa30b5db92d28fb435ba446b8aa1d10e134b4
karthick <karthylock@gmail.com>
2021-07-02 10:01:51 +0000

    practiced

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
 class MyArray{
-
   constructor(){
+    this.data = {}
     this.length = 0;
-    this.data = {};
   }
 
- get(item){
-  return this.data[item];
- }
+  get(index){
+    var item = this.data[index];
+    return item;
+  }
 
- push(val){
-   this.data[this.length] = val;
-   this.length++;
-   return this.length;
- }
+  push(item){
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
 
- pop(){
-   const lastItem = this.data[this.length-1];
-   delete this.data[this.length-1];
-   this.length--;
-   return lastItem;
- }
+  pop(index){
+    var lastItem = this.data[this.length-1];
+    delete this.data[this.length-1];
+    this.length--;
+    return lastItem;
+  }
+   
+  delete(item){
+    const lastItem = this.data[this.length-1];
+    this.shiftItems(item);
+    return lastItem;
+  }
 
- delete(item){
-   const lastItem = this.data[this.length-1];
-   this.shiftItems(item);
-   return lastItem;
- }
-
- shiftItems(item){
-   for(let i = item; i < this.length - 1; i++){
-     this.data[i] = this.data[i + 1];
-   }
-     delete this.data[this.length - 1];
-     this.length--;
- }
+  shiftItems(item){
+    for(let i = item; i < this.length-1; i++){
+      this.data[i] = this.data[i+1];
+    }
+    delete this.data[this.length-1];
+    this.length--;
+  }
 }
 
 const newArr = new MyArray();
+newArr.push('hi');
 newArr.push('hello');
-newArr.push('i think');
-newArr.push('it will work');
-newArr.push('i ');
-// newArr.delete(1);
+newArr.push('how');
+newArr.push('are');
+newArr.push('you');
+// newArr.pop();
+// newArr.pop();
 newArr.delete(2);
-newArr.delete();
+newArr.delete(3);
+newArr.delete(1);
+newArr.delete(0);
 console.log(newArr);
